35d2e028fd0b76ae1025f61ad6cf2ffdaf8a85c6
debDiGi <mya.fsd7@gmail.com>
2024-02-13 18:57:33 +0100

    update2 view e controller

--- src/main/java/it/corso/controller/RiservataController.java
+++ src/main/java/it/corso/controller/RiservataController.java
@@ -1,6 +1,9 @@
 package it.corso.controller;
 
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import it.corso.model.Attore;
+import it.corso.model.Film;
 import it.corso.service.AttoreService;
+import it.corso.service.FilmService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -23,6 +28,8 @@ public class RiservataController
 {
 	@Autowired
 	private AttoreService attoreService;
+	@Autowired
+	private FilmService filmService;
 	
 	@GetMapping
 	public String getPage(
@@ -30,6 +37,8 @@ public class RiservataController
 			HttpSession session
 			) 
 	{
+		Map<String, List<Film>> filmsMap = filmService.getFilmsByGenere(filmService.getFilms());
+		model.addAttribute("filmsMap", filmsMap);
 		Attore attore = (Attore) session.getAttribute("attore");
 		model.addAttribute("attore", attore);
 		return "riservata";

f8ec7187f6e98f7ac3fdadf7f4eb3204a66d511a
debDiGi <mya.fsd7@gmail.com>
2024-02-13 18:11:57 +0100

    varie view e controller

--- src/main/java/it/corso/controller/DettaglioAttoreController.java
+++ src/main/java/it/corso/controller/DettaglioAttoreController.java
@@ -1,11 +1,17 @@
 package it.corso.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import it.corso.model.Film;
 import it.corso.service.AttoreService;
+import it.corso.service.FilmService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,11 +22,15 @@ public class DettaglioAttoreController {
 
 	@Autowired
 	private AttoreService attoreService;
+	@Autowired
+	private FilmService filmService;
 	
 	@GetMapping
 	public String getPage(
 			@RequestParam ("id")int id,
 			Model model) {
+		Map<String, List<Film>> filmsMap = filmService.getFilmsByGenere(filmService.getFilms());
+		model.addAttribute("filmsMap", filmsMap);
 		model.addAttribute("attore", attoreService.getAttoreById(id));
 		model.addAttribute("filmografia", attoreService.getFilmografia(id));
 		return "dettaglioattore";
